b69df1af87886f8dcefdb9ec7193c474924a3dae
methokb <abdurasulovrahmatso@gmail.com>
2022-12-11 15:33:45 +0300

    dz3

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+// function count (number1, number2){
+//     return Math.min(number1, number2)
+// }
+// console.log(count(8,15));
+
+
+// function twoMass(a, b) {
+//     console.log(arguments[0]);
+//     console.log(arguments[1]);  
+//   }
+//   twoMass(1, 2);
+
+
+const str = "я стану лидером"
+console.log(str.length);
+
+
